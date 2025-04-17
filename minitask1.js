0de8d1bc9cc440f95718ca7faa84e9f9f4129c97
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-17 11:37:52 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,12 @@
+function oddEven(X) {
+    if (X > 0) {
+        if (X%2==0) {
+            console.log(`${X} adalah bilangan genap`)
+        } else {
+            console.log(`${X} adalah bilangan ganjil`)
+        }
+    }
+}
+
+oddEven(2);
+oddEven(3);
